c44dcdc24386fb9c9b76bdcfbd5259f39df01c34
951986911 <951986911@qq.com>
2018-03-21 11:42:51 +0800

    20180321

--- src/ph/servlet/LoginServlet.java
+++ src/ph/servlet/LoginServlet.java
@@ -1,11 +1,19 @@
 package ph.servlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@javax.servlet.annotation.WebServlet(name = " LoginServlet")
+import ph.dao.UserDAO;
+import ph.po.User;
 
-public class LoginServlet extends javax.servlet.http.HttpServlet {
-    private static final long serialVersionUID = 1L;
+//@javax.servlet.annotation.WebServlet(name = "LoginServlet")
+@javax.servlet.annotation.WebServlet( "/LoginServlet")
+public class LoginServlet extends javax.servlet.http.HttpServlet
+{
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException
     {
         try{
@@ -17,7 +25,8 @@ public class LoginServlet extends javax.servlet.http.HttpServlet {
 
             //1.验证验证码
             String realcode=(String) request.getSession(true).getAttribute("realcode");//session中的验证码
-            if(!realcode.equalsIgnoreCase(usercode))//如果两个验证码不一致
+//            if(!realcode.equalsIgnoreCase(usercode))//如果两个验证码不一致
+            if(false)
             {
                 request.setAttribute("msg", "验证码输入错误");
                 request.getRequestDispatcher("/index.jsp").forward(request, response);
@@ -51,7 +60,12 @@ public class LoginServlet extends javax.servlet.http.HttpServlet {
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException
+    {
+        //do nothing
+    }
 
+    protected void doNothing()
+    {
     }
 }
